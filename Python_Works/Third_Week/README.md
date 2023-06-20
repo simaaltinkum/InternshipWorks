8a4b67c85b00a0b5959866cb6057520574b1501e
Sima Altınkum <simaaltinkum02@gmail.com>
2023-06-20 15:17:49 +0300

    Updated

---
+++ Python_Works/Third_Week/README.md
@@ -0,0 +1,4 @@
+It provides follow your employees entering and exit times.
+Firstly i imported datetime, timedelta and random. I created lastTime and firstTime variable for buffering beginning and end date. "c" variable calculates the difference between last date and beginning date.
+"time" function buffers dataArr for adding new entering and exit times "c.days" times. Also if it is weekend time it prints it. If it is not weekend random assigns hour and minute for per section of a day. End of this function i return dataArr string form because write it txt folder. Lists are not valid for txt write operation.
+Finally i created a txt folder for recording datas. I write it write() method. Then, i used read() method to open it python for read.
--- Python_Works/Third_Week/employeeTrackingSystem.py
+++ Python_Works/Third_Week/employeeTrackingSystem.py
@@ -1,18 +1,21 @@
 from datetime import datetime, timedelta
 import random
+firstTime = input("Write beginning date and moment: (yyyy/mm/dd/hh/min/sec)")
+lastTime = input("Write end date and moment: ")
 
-lastTime = datetime(2023, 6, 19, 15, 25, 30) # this is last date which is we want to see until it
-firstTime = datetime(2023, 5, 14, 8, 21, 10) # this is beggining date which is we want to see
+firstTime = datetime.strptime(firstTime, "%Y/%m/%d/%H/%M/%S")
+lastTime = datetime.strptime(lastTime, "%Y/%m/%d/%H/%M/%S")
 
-c = lastTime - firstTime # it calculates how many days are begining between last
+c = lastTime - firstTime
 print("Difference:", c.days)
+
 dataArr = []
 def time():
     dataArr = [] # i create array to buffer the entering and exit datas
     for i in range(c.days): 
         k = 0
         if i == 5 + 7*k or i == 6 + 7*k: # i write this code 19 june 2023 monday i test it with this date if u want to change day you must change 5 and 6 
-            wknd = "Weekend\t"
+            wknd = ""
             k += 1
             dataArr.append(wknd)
             
@@ -24,21 +27,28 @@ def time():
             
             lunchHour = random.randint(12, 13)
             lunchMinute = random.randint(0, 59)
+
+            lunchEnterHour = random.randint(12, 13)
+            lunchEnterMinute = random.randint(0, 59)
                 
             outHour = random.randint(17, 24)
             outMinute = random.randint(0, 59)
 
             actualDate = firstTime + timedelta(days=i) # timedelta ensure that change days
-            
-            data = f"Sahin Mersin", actualDate.strftime("%Y-%m-%d"), "Morning:", f"{enterHour:02d}:{enterMinute:02d}", "Lunch:", f"{lunchHour:02d}:{lunchMinute:02d}", "Exit:", f"{outHour:02d}:{outMinute:02d}\t"
+            hour = (lunchHour - enterHour) + (outHour - lunchEnterHour)
+            data = f"Sahin Mersin", actualDate.strftime("%Y-%m-%d"), "Morning:", f"{enterHour:02d}:{enterMinute:02d}", "Lunch Exit:", f"{lunchHour:02d}:{lunchMinute:02d}","Lunch Enter:",f"{lunchEnterHour:02d}:{lunchEnterMinute}", "Exit:", f"{outHour:02d}:{outMinute:02d}","Total work: ",f"{hour}","hours"
             dataArr.append(data)
 
-    return str(dataArr)
+            
+
+    return dataArr
 
 # print(time())
             
 employeeTracking = open("employeeTracking.txt", "w") # i created a txt doc for recording datas
-employeeTracking.write(time())
+listemiz = time()
+for i in listemiz:
+    employeeTracking.write(str(i) + "\n")
 employeeTracking.close()
             
 employeeTracking = open("employeeTracking.txt", "r")

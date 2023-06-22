a4758b997d0e334091ef4fae86911052c1333942
Sima Altınkum <simaaltinkum02@gmail.com>
2023-06-22 09:52:50 +0300

    Updated

--- Python_Works/Third_Week/Url_Retrieve/photo.py
+++ Python_Works/Third_Week/Url_Retrieve/photo.py
@@ -1,10 +1,21 @@
 import webbrowser
+import urllib.request
 
-with open("resimler.txt", "r") as f:
+with open("photo.txt", "r") as f:
     urls = f.readlines()
 
 for url in urls:
     url = url.strip()
+
+    try:
+        response = urllib.request.urlopen(url)
+        file_name = url.split("/")[-1] 
+        with open(file_name, "wb") as file:
+            file.write(response.read())
+        print(f"Downloaded: {file_name}")
+    except Exception as e:
+        print(f"Failed to download: {url}, Error: {e}")
     webbrowser.open_new_tab(url)
+    
 
 

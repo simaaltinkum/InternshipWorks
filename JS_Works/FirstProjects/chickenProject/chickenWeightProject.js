3e39c66123bef1e81e989346fa67780043fdcf27
Sima Altınkum <simaaltinkum02@gmail.com>
2023-07-06 11:38:01 +0300

    Updated

--- JS_Works/FirstProjects/chickenProject/chickenWeightProject.js
+++ JS_Works/FirstProjects/chickenProject/chickenWeightProject.js
@@ -7,21 +7,21 @@ function calculate() {
 var total = 0;
 var totalChicken = 0;
 
-for (var i = 1; i <= day; i++) {
-  var chickenRand = Math.floor(Math.random() * chickenNum + 1);
-  console.log(i + ". day " + chickenRand + " chicken(s) measured.");
-  totalChicken += chickenRand;
+  for (var i = 1; i <= day; i++) {
+    var chickenRand = Math.floor(Math.random() * chickenNum + 1);
+    console.log(i + ". day " + chickenRand + " chicken(s) measured.");
+    totalChicken += chickenRand;
 
-  for (var j = 1; j <= chickenRand; j++) {
-    let weightRand = Math.floor(Math.random() * (chickenWeightMax));
-    console.log(j + ". chicken weight: " + weightRand);
-    total += weightRand;
+    for (var j = 1; j <= chickenRand; j++) {
+      let weightRand = Math.floor(Math.random() * (chickenWeightMax));
+      console.log(j + ". chicken weight: " + weightRand);
+      total += weightRand;
+    }
+
+    console.log("Uploaded total weight: " + total);
+    console.log("Average weight: " + total / totalChicken);
+
   }
-
-  console.log("Uploaded total weight: " + total);
-  console.log("Average weight: " + total / totalChicken);
-
-}
 }
 
 calculate();

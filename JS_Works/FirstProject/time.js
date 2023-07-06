f0e1184cc7aaffb70fdf0a37f6da9b8b28990bc0
Sima Altınkum <109222132+simaaltinkum@users.noreply.github.com>
2023-07-06 09:47:42 +0300

    Delete JS_Works/FirstProject directory

--- JS_Works/FirstProject/time.js
+++
@@ -1,8 +0,0 @@
-function updateTime() {
-    var date = new Date().toLocaleString("tr-TR");
-    document.getElementById("time").innerHTML = "Time right now: " + date;
-}
-
-updateTime(); // Call the function initially to display the time immediately
-
-setInterval(updateTime, 1000); // Call the function every second to update the time

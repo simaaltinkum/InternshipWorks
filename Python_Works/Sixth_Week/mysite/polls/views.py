c38869914040917e56b671531e7bcd95e9dc1b58
Sima Altınkum <simaaltinkum02@gmail.com>
2023-07-13 15:05:04 +0300

    Updated

--- Python_Works/Sixth_Week/mysite/polls/views.py
+++ Python_Works/Sixth_Week/mysite/polls/views.py
@@ -1,7 +1,9 @@
 from django.http import HttpResponse
 from .models import Question
 from django.http import Http404
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404,render
+from django.http import HttpResponse
+from django.template import loader
 
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
@@ -14,12 +16,7 @@ def results(request, question_id):
     return HttpResponse(response % question_id)
 
 def vote(request, question_id):
-    return HttpResponse("You're voting on question %s" % question_id) 
-from django.http import HttpResponse
-from django.template import loader
-
-from .models import Question
-
+    return HttpResponse("You're voting on question %s" % question_id)
 
 def index(request):
     latest_question_list = Question.objects.order_by("-pub_date")[:5]

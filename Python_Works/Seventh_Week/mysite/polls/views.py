e443d51617916dafa9077827e8955eceab432873
Sima Altınkum <simaaltinkum02@gmail.com>
2023-07-20 14:43:11 +0300

    Updated

--- Python_Works/Seventh_Week/mysite/polls/views.py
+++ Python_Works/Seventh_Week/mysite/polls/views.py
@@ -20,10 +20,13 @@ class DetailView(generic.DetailView):
 class ResultsView(generic.DetailView):
     model = Question
     template_name = "polls/results.html"
+
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
+
     try:
         selected_choice = question.choice_set.get(pk=request.POST["choice"])
+
     except (KeyError, Choice.DoesNotExist):
         return render(
             request,
@@ -33,6 +36,7 @@ def vote(request, question_id):
                 "error_message":"You didn't select a choice"
             },
         )
+
     else:
         selected_choice.votes += 1
         selected_choice.save()

63f44f6bd7dcdb10af25c753e82b97c8cda46e47
Sima Altınkum <simaaltinkum02@gmail.com>
2023-07-20 16:15:10 +0300

    Updated

--- Python_Works/Seventh_Week/mysite/polls/tests.py
+++ Python_Works/Seventh_Week/mysite/polls/tests.py
@@ -6,6 +6,10 @@ from django.urls import reverse
 
 from .models import Question
 
+def create_question(question_text, days):
+    time = timezone.now() + datetime.timedelta(days = days)
+    return Question.objects.create(question_text = question_text, pub_date = time)
+
 class QuestionModelTests(TestCase):
     def test_was_published_recently_with_future_question(self):
         time = timezone.now() + datetime.timedelta(days=30)
@@ -21,3 +25,16 @@ class QuestionModelTests(TestCase):
         time =timezone.now() - datetime.timedelta(hours=23, minutes=59, seconds=59)
         recent_question = Question(pub_date=time)
         self.assertIs(recent_question.was_published_recently(), True)
+
+    class QuestionDetailViewTests(TestCase):
+        def test_future_question(self):
+            future_question = create_question(question_text ="Future question.", days=5)
+            url = reverse("polls:detail", args=(future_question.id,))
+            response = self.client.get(url)
+            self.assertEqual(response.status_code, 404)
+
+        def test_past_question(self):
+            past_question = create_question(question_text="Past Question.", days=-5)
+            url = reverse("polls:detail", args = (past_question.id,))
+            response = self.client.get(url)
+            self.assertContains(response, past_question.question_text)

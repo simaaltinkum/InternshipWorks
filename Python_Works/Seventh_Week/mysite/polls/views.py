a8190192ed66b822ab326c9738dab16ccc384a58
Sima Altınkum <simaaltinkum02@gmail.com>
2023-07-20 15:46:02 +0300

    Updated

--- Python_Works/Seventh_Week/mysite/polls/views.py
+++ Python_Works/Seventh_Week/mysite/polls/views.py
@@ -1,10 +1,11 @@
-from django.http import HttpResponse, HttpResponseRedirect
-from .models import Choice, Question
+from django.http import HttpResponse, HttpResponseRedirect, Http404
+from django.utils import timezone
 from django.shortcuts import get_object_or_404, render
 from django.template import loader
-from django.http import Http404
 from django.urls import reverse
 from django.views import generic
+
+from .models import Choice, Question
 
 class IndexView(generic.ListView):
     template_name = "polls/index.html"

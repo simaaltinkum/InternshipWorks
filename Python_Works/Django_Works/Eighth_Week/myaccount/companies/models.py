7156b1041ba693206a008956f3f67fcf0c8f7a44
Sima Altınkum <simaaltinkum02@gmail.com>
2023-08-03 10:16:34 +0300

    Updated

--- Python_Works/Django_Works/Eighth_Week/myaccount/companies/models.py
+++ Python_Works/Django_Works/Eighth_Week/myaccount/companies/models.py
@@ -10,15 +10,3 @@ class Customer(models.Model):
     name = models.CharField(max_length=200)
     debt = models.IntegerField(default=0)
     credit = models.IntegerField(default=0)
-
-"""data = Companies(company_name="Meşe Bilişim", boss_name="Şahin Mersin", debt=1000, credit=500)
-data.save()
-
-data2 = Customer(name="Sima Altınkum", debt=100, credit=1200)
-data2.save()
-
-data =  Companies.objects.get(pk=1)
-data.delete()
-
-data2 = Customer.objects.get(pk=1)
-data2.delete()"""
--- Python_Works/Django_Works/Eighth_Week/myaccount/myaccount/urls.py
+++ Python_Works/Django_Works/Eighth_Week/myaccount/myaccount/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path("companies/", include("companies.urls")),
+    path("", include("companies.urls")),
     # path('admin/', admin.site.urls),
 ]

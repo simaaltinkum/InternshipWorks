f692f3087134786e2358e4fc4de4472e5f1c2a32
Sima Altınkum <simaaltinkum02@gmail.com>
2023-08-05 21:37:34 +0300

    Updated

--- Python_Works/Django_Works/Eighth_Week/project/courseapp/pages/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .import views
-
-urlpatterns = [
-    path('', views.index ),
-    path('index',views.index),
-    path('contact',views.contact),
-    path('about',views.about),
-]

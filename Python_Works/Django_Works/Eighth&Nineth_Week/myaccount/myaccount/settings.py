9a76e02a9d886dd4aafd106dd9492d0e2cf1b1fc
Sima Altınkum <simaaltinkum02@gmail.com>
2023-08-07 10:34:51 +0300

    Updated

--- Python_Works/Django_Works/Eighth&Nineth_Week/myaccount/myaccount/settings.py
+++ Python_Works/Django_Works/Eighth&Nineth_Week/myaccount/myaccount/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-g0%&q1m7xt+&1daoot7!z-_7@$^_i6m!&pmkq1)3-*f6o)$$p4
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -119,6 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [BASE_DIR / 'static/',]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field

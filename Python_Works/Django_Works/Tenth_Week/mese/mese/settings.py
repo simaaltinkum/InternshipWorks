5a6f20af24d4d6f198e7b070c19b90df39b0aa4a
Sima Altınkum <simaaltinkum02@gmail.com>
2023-08-23 09:41:33 +0300

    Updated

--- Python_Works/Django_Works/Tenth_Week/mese/mese/settings.py
+++ Python_Works/Django_Works/Tenth_Week/mese/mese/settings.py
@@ -83,8 +83,13 @@ WSGI_APPLICATION = 'mese.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'simadb',
+        'USER':'root',
+        'PASSWORD': '********',
+        "DEFAULT-CHARACTER-SET": 'utf8mb4',
+        'HOST':'localhost',
+        'PORT': '3306'
     }
 }
 

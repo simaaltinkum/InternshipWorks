dfeeb62630a883416ee443b1e8d624ce7adc16cd
Sima Altınkum <simaaltinkum02@gmail.com>
2023-09-26 10:07:30 +0300

    Updated

--- Python_Works/Django_Works/Seventeenth_Week/qr_reader/qr_app/views.py
+++ Python_Works/Django_Works/Seventeenth_Week/qr_reader/qr_app/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from pyzbar.pyzbar import decode
 import cv2
+from numpy import *
+from cv2 import *
 from .models import Qr
 from .forms import QrForm
 
@@ -13,7 +15,9 @@ def read_qr(request):
 
     while data is None:
         ret, frame = cap.read()
+
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
         try:
             decoded_objects = decode(gray)
 
@@ -21,14 +25,24 @@ def read_qr(request):
                 data = obj.data.decode('utf-8')
                 (x, y, w, h) = obj.rect
                 cv2.rectangle(gray, (x, y), (x + w, y + h), (255, 0, 0), 2)
+                print("barkod")
 
                 if obj.data != "":
                     print(obj.data)
                     print(obj.type)
+
+                    if ret:
+                        cv2.imshow("saved", frame)
+                        cv2.imwrite("savedcv.png", frame)
+                        cv2.waitKey(0)
+                        cv2.destroyWindow("saved")
                 break
+                return render(request, 'qr_reader.html')
 
         except Exception as e:
             print("Barkod okuma hatası:", str(e))
+
+
 
         if data is not None:
             break
@@ -44,6 +58,14 @@ def read_qr(request):
 
         return render(request, 'qr_reader.html', {'data': data})
 
+    if data is not None:
+        cv2.destroyAllWindows()
+
+        try:
+            qr = Qr.objects.get(qr=data)
+            return render(request, 'qr_reader.html', {'data': qr.qr})
+        except Qr.DoesNotExist:
+            return render(request, 'no_qr_found.html')
     else:
         cv2.destroyAllWindows()
         return render(request, 'no_qr_found.html')

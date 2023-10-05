f43f56a14266718a03fda5a884eceec78fc519ac
Sima Altınkum <simaaltinkum02@gmail.com>
2023-10-05 14:29:27 +0300

    Updated

--- Python_Works/Django_Works/Seventeenth_Week/qr_reader/qr_app/views.py
+++ Python_Works/Django_Works/Seventeenth_Week/qr_reader/qr_app/views.py
@@ -15,28 +15,28 @@ def homepage(request):
 
 
 def read(request):
-    cap = cv2.VideoCapture(0)
+    cap = cv2.VideoCapture(0) # Open camera with openCV
     qr = None
     image = ''
     frame = None
 
-    while qr is None:
-        ret, frame = cap.read()
+    while qr is None: # If qr or barcode empty
+        ret, frame = cap.read() # Returns boolean if the frame is read correctly, it will be true
 
         if ret:
-            image = os.path.join(f"qr_{str(uuid.uuid4())[:8]}.png")
-            cv2.imwrite(MEDIA_ROOT + "/" + image, frame)
+            image = os.path.join(f"qr_{str(uuid.uuid4())[:8]}.png") # Naming the image which is photogrgaphed
+            cv2.imwrite(MEDIA_ROOT + "/" + image, frame) # Saving the  image
             break
 
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY) # Conversion code
 
     try:
-        decoded_objects = decode(gray)
+        decoded_objects = decode(gray) # Returns decoded string
 
         for obj in decoded_objects:
             qr = obj.data.decode('utf-8')
-            (x, y, w, h) = obj.rect
-            cv2.rectangle(gray, (x, y), (x + w, y + h), (255, 0, 0), 2)
+            (x, y, w, h) = obj.rect # Locating the barcode
+            cv2.rectangle(gray, (x, y), (x + w, y + h), (255, 0, 0), 2) #Highlight the barcode
 
             if obj.qr != "":
                 print(obj.qr)
@@ -45,20 +45,20 @@ def read(request):
     except Exception as e:
         print("Barcode reading fault:", str(e))
 
-    cap.release()
+    cap.release() # Closing all windows
     cv2.destroyAllWindows()
     # return render(request,'read.html')
 
     if qr and image is not None:
-        object = Qr.objects.create(qr =qr, image= image)
-        return HttpResponseRedirect(reverse('save', args=[object.pk]))
+        object = Qr.objects.create(qr =qr, image= image) # Saving decoded QR or Barcode code and image
+        return HttpResponseRedirect(reverse('save', args=[object.pk])) # Directing saving page
 
     else:
         return HttpResponse('No QR or Barcode found')
 
 
 def save(request, pk):
-    if request.method == 'GET':
+    if request.method == 'GET': # Informations comes when you in get request
         object = Qr(pk=pk)
 
         initial_dict = {
@@ -74,7 +74,7 @@ def save(request, pk):
             'image': object.image,
             'pk': pk
         }
-    if request.method == 'POST':
+    if request.method == 'POST': # Pk comes otomatically enter type
         form = QrForm(request.POST)
 
         if form.is_valid():
@@ -83,7 +83,7 @@ def save(request, pk):
 
         object= Qr.objects.get(pk=pk)
         object.type = type
-        object.save()
+        object.save() # Type saving
         context = {}
 
     return render(request, 'save.html', context)
@@ -91,9 +91,9 @@ def save(request, pk):
 
 def search(request):
     if request.method == "POST":
-        searched = request.POST['searched']
-        qr = Qr.objects.filter(qr = searched)
-        type = Qr.objects.filter(type = searched)
+        searched = request.POST['searched'] # Post what you want to search (qr, barcode or type)
+        qr = Qr.objects.filter(qr = searched) # Qr query to equallity
+        type = Qr.objects.filter(type = searched) # Type query to equallity
         return render(request, 'search.html', {'searched': searched, 'qr': qr, 'type': type})
 
     else:
@@ -101,7 +101,7 @@ def search(request):
 
 
 def list(request):
-    qr_list = Qr.objects.all()
+    qr_list = Qr.objects.all() #Retrieving data from database to list it
 
     context = {
         'qr_list': qr_list

a0e58ad58e0f1c8aaddee3c51a6e588d11fcd69e
Sima Altınkum <simaaltinkum02@gmail.com>
2023-10-05 08:09:58 +0300

    Updated

--- Python_Works/Django_Works/Eleventh_Week/sendemail/sendemail/settings.py
+++ Python_Works/Django_Works/Eleventh_Week/sendemail/sendemail/settings.py
@@ -81,7 +81,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'email',
         'USER':'root',
-        'PASSWORD': 'NEdeyon2',
+        'PASSWORD': '********',
         "DEFAULT-CHARACTER-SET": 'utf8mb4',
         'HOST':'localhost',
         'PORT': '3306'

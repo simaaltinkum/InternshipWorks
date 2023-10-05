95d701800082fd40e9138ec51d63d383611b0cf2
Sima Altınkum <simaaltinkum02@gmail.com>
2023-10-05 13:44:40 +0300

    Updated

--- Python_Works/Django_Works/Seventeenth_Week/qr_reader/qr_app/models.py
+++ Python_Works/Django_Works/Seventeenth_Week/qr_reader/qr_app/models.py
@@ -1,6 +1,9 @@
 from django.db import models
+from django.utils import timezone
+import datetime
 
 class Qr(models.Model):
     qr = models.CharField(max_length=170)
     type = models.CharField(max_length=170)
     image = models.ImageField(upload_to='media')
+    created_at = models.DateTimeField(auto_now_add=True)

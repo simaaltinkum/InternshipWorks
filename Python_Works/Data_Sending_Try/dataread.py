e113498dda27d10f908d7c5f5455904ce5efde73
Sima Altınkum <simaaltinkum02@gmail.com>
2023-10-18 14:07:15 +0300

    Updated

--- Python_Works/Data_Sending_Try/dataread.py
+++ Python_Works/Data_Sending_Try/dataread.py
@@ -1,12 +1,18 @@
 import requests
 
 # read api key
-API_KEY = '33cc8bb9a354204dc23900cc' # demo account #100 Demo Data HTTP Temperature sensor
+API_KEY = '3a124c81cfc08b5c3dfa28cc' # demo account #100 Demo Data HTTP Temperature sensor
 
 url = 'https://iothook.com/api/device/?api_key=' + API_KEY
 
 response = requests.get(url)
 data = response.json()
+
+file_path = 'doc.txt'
+
+with open(file_path, 'w') as file:
+    file.write(str(data))
+
 print(data)
 # print(data['datas'][0]['id'])
 # print(data['datas'][0]['pub_date'])
---
+++ Python_Works/Data_Sending_Try/datawrite.py
@@ -0,0 +1,3 @@
+with open('doc.txt', 'r') as file:
+    file_contents = file.read()
+    print(file_contents)

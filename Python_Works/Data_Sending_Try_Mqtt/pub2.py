b7db3ac671e8392dc1c47446101a503a7dea6cc8
Sima Altınkum <simaaltinkum02@gmail.com>
2023-10-20 11:16:51 +0300

    Updated

---
+++ Python_Works/Data_Sending_Try_Mqtt/pub2.py
@@ -0,0 +1,13 @@
+import sys
+import paho.mqtt.client as mqtt
+
+broker = "iothook.com"
+port = 1883
+topic = "simaltinkum"
+
+while True:
+    client = mqtt.Client()
+    client.connect(broker, port)
+    data = "try"
+    client.publish(topic, "%s" %data)
+    client.disconnect()

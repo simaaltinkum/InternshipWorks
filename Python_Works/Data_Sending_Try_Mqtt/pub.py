2c99cde7ab14cfbca252958d8473ae856c6a0b73
Sima Altınkum <simaaltinkum02@gmail.com>
2023-10-20 10:15:22 +0300

    Updated

--- Python_Works/Data_Sending_Try_Mqtt/pub.py
+++ Python_Works/Data_Sending_Try_Mqtt/pub.py
@@ -5,10 +5,18 @@ broker = "iothook.com"
 port = 1883
 topic = "simaltinkum"
 
-if __name__ == "__main__":
+def on_connect(client, userdata, flags, rc):
+    if rc == 0:
+        print("Connected to broker")
+    else:
+        print(f"Connection failed with code {rc}")
+
+while True:
     client = paho.Client()
+    client.on_connect = on_connect
     client.connect(broker, port)
+    client.loop_start()  # Start the MQTT loop
     t = int(time.time())
-    ret = client.publish(topic, "%s" % t)  # publish
+    ret = client.publish(topic, "%s" % t, qos=0, retain=False)  # publish
     print(ret)
     time.sleep(1)

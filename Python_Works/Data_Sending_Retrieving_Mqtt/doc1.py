e8ae90846ae537bc926d3edc34c263fb859509b5
Sima Altınkum <simaaltinkum02@gmail.com>
2023-10-23 12:00:19 +0300

    Updated

--- Python_Works/Data_Sending_Retrieving_Mqtt/doc1.py
+++ Python_Works/Data_Sending_Retrieving_Mqtt/doc1.py
@@ -18,8 +18,20 @@ def on_publish(client, userdata, result):
     print("on_publish")
 
 
+def on_connect(client, userdata, flags, rc):
+    print(f"Connection status: {rc}")
+    if rc == 0:
+        client.subscribe("test2")
+    else:
+        print("Connection failed.")
+
+
+def on_message(client, userdata, message):
+    print(f"Konu: {message.topic} - Mesaj: {message.payload}")
+
+
 def control(client, userdata, num):
-    if userdata == num+1 :
+    if userdata == num + 1 :
         print("True")
         ret = client.publish(topic, num)
         print(ret)
@@ -33,6 +45,8 @@ while True:
     client = paho.Client()
     client.username_pw_set("iothookpublic", "iothookpublic")
     client.on_publish = on_publish
+    client.on_connect = on_connect
+    client.on_message = on_message
     client.connect(broker, port)
     publish()
     # control()
--- Python_Works/Data_Sending_Retrieving_Mqtt/doc2.py
+++ Python_Works/Data_Sending_Retrieving_Mqtt/doc2.py
@@ -17,11 +17,12 @@ def on_connect(client, userdata, flags, rc):
 
 def on_message(client, userdata, message):
     print(f"Konu: {message.topic} - Mesaj: {message.payload}")
+    userdata += 1
 
 
 def publish(client, userdata):
     print("publish")
-    userdata = userdata + 1
+    # userdata = userdata + 1
     ret = client.publish(topic, userdata)
     print(ret)
     time.sleep(1)
@@ -30,15 +31,17 @@ def publish(client, userdata):
 def on_publish(client, userdata, result):
     print("on_publish")
 
+
 while True:
     client = mqtt.Client("clientId-HNOt0ZhMl3")
     client.username_pw_set("iothookpublic", "iothookpublic")
     client.on_message = on_message
     client.on_connect = on_connect
     client.on_publish = on_publish
-    publish()
+
     print("connecting to broker")
     client.connect(broker, port)
 
+    publish()
     client.subscribe("test2")
     client.loop_forever()

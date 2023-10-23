1fba9f30adfd87061a81d18c1430f35483b20e4a
Sima Altınkum <simaaltinkum02@gmail.com>
2023-10-23 10:26:13 +0300

    Updated

--- Python_Works/Data_Sending_Retrieving_Mqtt/doc2.py
+++ Python_Works/Data_Sending_Retrieving_Mqtt/doc2.py
@@ -3,20 +3,37 @@ import time
 
 broker = "iothook.com"
 port = 1883
+topic = "test2"
+
+
+def on_connect(client, userdata, flags, rc):
+    print(f"Bağlantı durumu: {rc}")
+    if rc == 0:
+        client.subscribe("test2")
+    else:
+        print("Bağlantı başarısız.")
+
 
 def on_message(client, userdata, message):
-    print("message received ", str(message.payload.decode("utf-8")))
-    print("message topic=", message.topic)
-    print("message qos=", message.qos)
-    print("message retain flag=", message.retain)
+    print(f"Konu: {message.topic} - Mesaj: {message.payload}")
 
 
-def publish():
-    num = num+1
+def increase(client, userdata):
+    userdata = userdata + 1
 
-client = mqtt.Client("client-id-subscribe-1234567")
+
+def publish(client, userdata):
+    print("publish")
+    ret = client.publish(topic, userdata)
+    print(ret)
+    time.sleep(1)
+
+
+
+client = mqtt.Client("clientId-HNOt0ZhMl3")
 client.username_pw_set("iothookpublic", "iothookpublic")
 client.on_message = on_message
+client.on_connect = on_connect
 
 print("connecting to broker")
 client.connect(broker, port)

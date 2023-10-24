ac30f187ae7a86fbd4003ea93785e5b7744be5b6
Sima Altınkum <simaaltinkum02@gmail.com>
2023-10-24 08:40:15 +0300

    Updated

--- Python_Works/Data_Sending_Retrieving_Mqtt/doc1.py
+++ Python_Works/Data_Sending_Retrieving_Mqtt/doc1.py
@@ -1,5 +1,6 @@
 import paho.mqtt.client as paho
 import time
+import threading
 
 broker = "iothook.com"
 port = 1883
@@ -8,15 +9,14 @@ print("s")
 
 def publish():
     print("publish")
-    num = input("Login a number: ")
-    ret = client.publish(topic, num)
-    print(ret)
-    time.sleep(1)
-
+    while True:
+        num = input("Login a number: ")
+        ret = client.publish(topic, num)
+        print(ret)
+        time.sleep(1)
 
 def on_publish(client, userdata, result):
     print("on_publish")
-
 
 def on_connect(client, userdata, flags, rc):
     print(f"Connection status: {rc}")
@@ -25,29 +25,36 @@ def on_connect(client, userdata, flags, rc):
     else:
         print("Connection failed.")
 
-
 def on_message(client, userdata, message):
-    print(f"Konu: {message.topic} - Mesaj: {message.payload}")
-
+    print(f"Subject: {message.topic} - Message: {message.payload}")
 
 def control(client, userdata, num):
-    if userdata == num + 1 :
+    if userdata == num + 1:
         print("True")
         ret = client.publish(topic, num)
         print(ret)
         time.sleep(1)
-
     else:
         print("False")
 
-
-while True:
+def mqtt_client_thread():
+    global client
     client = paho.Client()
     client.username_pw_set("iothookpublic", "iothookpublic")
     client.on_publish = on_publish
     client.on_connect = on_connect
     client.on_message = on_message
     client.connect(broker, port)
-    publish()
-    # control()
     client.loop_forever()
+
+
+mqtt_thread = threading.Thread(target=mqtt_client_thread)
+mqtt_thread.start()
+
+
+publish_thread = threading.Thread(target=publish)
+publish_thread.start()
+
+
+mqtt_thread.join()
+publish_thread.join()
--- Python_Works/Data_Sending_Retrieving_Mqtt/doc2.py
+++ Python_Works/Data_Sending_Retrieving_Mqtt/doc2.py
@@ -1,11 +1,11 @@
 import paho.mqtt.client as mqtt
 import time
-
+import threading
 
 broker = "iothook.com"
 port = 1883
 topic = "test2"
-
+userdata = 0
 
 def on_connect(client, userdata, flags, rc):
     print(f"Connection status: {rc}")
@@ -14,34 +14,37 @@ def on_connect(client, userdata, flags, rc):
     else:
         print("Connection failed.")
 
-
 def on_message(client, userdata, message):
-    print(f"Konu: {message.topic} - Mesaj: {message.payload}")
-    userdata += 1
+    print(f"Subject: {message.topic} - Message: {message.payload}")
 
-
-def publish(client, userdata):
+def publish(client):  
+    global userdata
     print("publish")
-    # userdata = userdata + 1
+    userdata += 1
     ret = client.publish(topic, userdata)
     print(ret)
     time.sleep(1)
 
-
 def on_publish(client, userdata, result):
     print("on_publish")
 
-
-while True:
+def mqtt_thread():
     client = mqtt.Client("clientId-HNOt0ZhMl3")
     client.username_pw_set("iothookpublic", "iothookpublic")
+    client.on_publish = on_publish
     client.on_message = on_message
     client.on_connect = on_connect
-    client.on_publish = on_publish
 
     print("connecting to broker")
     client.connect(broker, port)
-
-    publish()
     client.subscribe("test2")
-    client.loop_forever()
+    client.loop_start()
+
+    while True:
+        publish(client)
+        time.sleep(1)
+
+if __name__ == "__main__":
+    mqtt_thread = threading.Thread(target=mqtt_thread)
+    mqtt_thread.start()
+    mqtt_thread.join()

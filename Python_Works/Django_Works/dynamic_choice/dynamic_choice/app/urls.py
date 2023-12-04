2fafe98443cfae72123319fe1eaada71c8a5187d
Sima Altınkum <simaaltinkum02@gmail.com>
2023-12-04 12:10:29 +0300

    Updated

--- Python_Works/Django_Works/dynamic_choice/dynamic_choice/app/urls.py
+++ Python_Works/Django_Works/dynamic_choice/dynamic_choice/app/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('home/', views.home, name='home'),
+    path('get_subcategories/', views.get_subcategories, name='get_subcategories'),
 ]
--- Python_Works/Django_Works/dynamic_choice/dynamic_choice/app/views.py
+++ Python_Works/Django_Works/dynamic_choice/dynamic_choice/app/views.py
@@ -1,8 +1,22 @@
 from django.shortcuts import render
+from django.http import JsonResponse
 from .forms import MyForm
+from .models import Category, SubCategory
 
 def home(request):
     form = MyForm(request.POST or None)
     context = {'form': form}
     return render(request, 'home.html', context)
 
+
+def get_subcategories(request):
+    if request.method == 'GET':
+        category_id = request.GET.get('category_id')
+        if category_id is not None:
+            subcategories = SubCategory.objects.filter(category_id=category_id)
+            subcategory_list = [{'id': sub.id, 'name': sub.name} for sub in subcategories]
+            return JsonResponse({'subcategories': subcategory_list})
+        else:
+            return JsonResponse({'error': 'Category ID is missing'}, status=400)
+    else:
+        return JsonResponse({'error': 'Only GET method is allowed'}, status=405)

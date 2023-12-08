e11aa5c255413879125ad1c8a066c1c8c2499617
Sima Altınkum <simaaltinkum02@gmail.com>
2023-12-08 10:27:15 +0300

    Updated

--- Python_Works/Django_Works/dynamic_choice/dynamic_choice/app/admin.py
+++ Python_Works/Django_Works/dynamic_choice/dynamic_choice/app/admin.py
@@ -1,6 +1,36 @@
 from django.contrib import admin
 from .models import Category, SubCategory, Saved
+from import_export import resources
+from import_export.admin import ImportExportModelAdmin
 
-admin.site.register(Category)
-admin.site.register(SubCategory)
-admin.site.register(Saved)
+
+class CategoryResource(resources.ModelResource):
+    class Meta:
+        model = Category
+
+
+class CategoryAdmin(ImportExportModelAdmin):
+    resource_class = CategoryResource
+
+
+class SubCategoryResource(resources.ModelResource):
+    class Meta:
+        model = SubCategory
+
+
+class SubCategoryAdmin(ImportExportModelAdmin):
+    resource_class = SubCategoryResource
+
+
+class SavedResource(resources.ModelResource):
+    class Meta:
+        model = Saved
+
+
+class SavedAdmin(ImportExportModelAdmin):
+    resource_class = SavedResource
+
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(SubCategory, SubCategoryAdmin)
+admin.site.register(Saved, SavedAdmin)

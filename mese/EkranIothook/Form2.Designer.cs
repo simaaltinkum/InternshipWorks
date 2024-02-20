4d8539af4f679ba95f5ab1bac8ddeef0e8a4faa2
Sima Altınkum <simaaltinkum02@gmail.com>
2024-02-20 16:24:12 +0300

    update

--- mese/EkranIothook/Form2.Designer.cs
+++ mese/EkranIothook/Form2.Designer.cs
@@ -28,18 +28,23 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
+            this.chart2 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            ((System.ComponentModel.ISupportInitialize)(this.chart2)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.label1.Location = new System.Drawing.Point(34, 94);
+            this.label1.Location = new System.Drawing.Point(34, 173);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(192, 29);
             this.label1.TabIndex = 0;
@@ -49,7 +54,7 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.label2.Location = new System.Drawing.Point(34, 187);
+            this.label2.Location = new System.Drawing.Point(34, 278);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(133, 29);
             this.label2.TabIndex = 1;
@@ -57,35 +62,54 @@
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(232, 101);
+            this.textBox1.Location = new System.Drawing.Point(232, 180);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(148, 22);
+            this.textBox1.Size = new System.Drawing.Size(176, 22);
             this.textBox1.TabIndex = 2;
             this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(232, 187);
+            this.textBox2.Location = new System.Drawing.Point(232, 285);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(148, 22);
+            this.textBox2.Size = new System.Drawing.Size(176, 22);
             this.textBox2.TabIndex = 3;
             this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(382, 231);
+            this.button1.Location = new System.Drawing.Point(304, 338);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(104, 26);
             this.button1.TabIndex = 4;
             this.button1.Text = "Ara";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // chart2
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart2.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart2.Legends.Add(legend1);
+            this.chart2.Location = new System.Drawing.Point(498, 75);
+            this.chart2.Name = "chart2";
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Pie;
+            series1.Legend = "Legend1";
+            series1.Name = "Yanma Sayısı";
+            this.chart2.Series.Add(series1);
+            this.chart2.Size = new System.Drawing.Size(450, 377);
+            this.chart2.TabIndex = 6;
+            this.chart2.Text = "chart2";
+            this.chart2.Click += new System.EventHandler(this.chart2_Click);
+            // 
             // Form2
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1004, 499);
+            this.Controls.Add(this.chart2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textBox1);
@@ -94,6 +118,7 @@
             this.Name = "Form2";
             this.Text = "Form2";
             this.Load += new System.EventHandler(this.Form2_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.chart2)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -106,5 +131,6 @@
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart2;
     }
 }
--- mese/EkranIothook/Form2.cs
+++ mese/EkranIothook/Form2.cs
@@ -9,6 +9,7 @@ using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
 using System.Xml;
 using System.Xml.Linq;
 
@@ -23,19 +24,16 @@ namespace EkranIothook
 
         private async void button1_Click(object sender, EventArgs e)
         {
-
-            // DateTime startDate;
-            // DateTime endDate;
-
-            string startDate = textBox1.Text;
-            string endDate = textBox2.Text;
-            string apiKey = "1c1094835fe305ad04096223";
-
-            string apiUrl = $"https://iothook.com/api/device/?api_key=1c1094835fe305ad04096223";
-
-            using (HttpClient httpClient = new HttpClient())
+            try
             {
-                try
+                string apiKey = "1c1094835fe305ad04096223";
+                string startDate = textBox1.Text; // Başlangıç tarihi
+                string endDate = textBox2.Text; // Bitiş tarihi
+
+                // API isteği için URL oluştur
+                string apiUrl = $"https://iothook.com/api/device/?api_key={apiKey}&start_date={startDate}&end_date={endDate}";
+
+                using (HttpClient httpClient = new HttpClient())
                 {
                     HttpResponseMessage response = await httpClient.GetAsync(apiUrl);
 
@@ -43,37 +41,24 @@ namespace EkranIothook
                     {
                         string responseData = await response.Content.ReadAsStringAsync();
 
-                        MessageBox.Show(responseData);
+                        // Verileri görselleştirme işlemi için ayrı bir metodu çağır
+                        ProcessResponseData(responseData);
                     }
                     else
                     {
                         MessageBox.Show("HTTP Error: " + response.StatusCode);
                     }
                 }
-                catch (Exception ex)
-                {
-                    MessageBox.Show("Error: " + ex.Message);
-                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
             }
 
-
-
-            Form3 fm3 = new Form3();
-            fm3.Show();
-
         }
 
-        private void textBox1_TextChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void textBox2_TextChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private async void Form2_Load(object sender, EventArgs e)
+        // HttpResponse verisini işlemek için ayrı bir metot
+        private async void ProcessResponseData(string responseData)
         {
             HttpClient httpClient = new HttpClient();
 
@@ -93,10 +78,32 @@ namespace EkranIothook
             string field_5 = member.field_5;
             string field_6 = member.field_6;
 
-            MessageBox.Show("Kırmızı: " + field_4 + " Yeşil: " + field_5 + " Sarı: " + field_6);
+            double total = Convert.ToDouble(field_4) + Convert.ToDouble(field_5) + Convert.ToDouble(field_6);
+
+            int percentageField4 = (int)((Convert.ToDouble(field_4) / total) * 100);
+            int percentageField5 = (int)((Convert.ToDouble(field_5) / total) * 100);
+            int percentageField6 = (int)((Convert.ToDouble(field_6) / total) * 100);
+
+            chart2.Series["Yanma Sayısı"].Points.Add(new DataPoint(0, Convert.ToDouble(field_4)) { AxisLabel = $"Kırmızı (%{percentageField4})" });
+            chart2.Series["Yanma Sayısı"].Points.Add(new DataPoint(0, Convert.ToDouble(field_5)) { AxisLabel = $"Yeşil (%{percentageField5})" });
+            chart2.Series["Yanma Sayısı"].Points.Add(new DataPoint(0, Convert.ToDouble(field_6)) { AxisLabel = $"Sarı (%{percentageField6})" });
+        }
+
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private async void Form2_Load(object sender, EventArgs e)
+        {
+           
             
-            // JSON formatına dönüştürülmüş veriyi göster
-            //  MessageBox.Show(responseString);
         }
 
         public class IothookMember
@@ -121,19 +128,12 @@ namespace EkranIothook
                 string responseString = await response.Content.ReadAsStringAsync();
                 var statusCode = response.StatusCode;
 
-            //     MessageBox.Show(responseString);
-
-                // string jsonString = "{\"Name\":\"John Doe\",\"Bio\":\"Software developer\",\"JoinDate\":\"2023-05-31T20:29:33-04:00\",\"Author\":true}";
-                // IothookMember member = JsonSerializer.Deserialize<IothookMember>(jsonString);
-
-
-            //     Console.WriteLine($"Field1: {member.Field1}");
-               // Console.WriteLine($"Field2: {member.Field2}");
-               // Console.WriteLine($"Field3: {member.Field3}");
             }
+        }
+
+        private void chart2_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
-
-
-

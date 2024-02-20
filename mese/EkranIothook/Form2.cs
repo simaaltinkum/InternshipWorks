d3f022a175a1c7909c2dd79fdddc90d9d821f438
Sima Altınkum <simaaltinkum02@gmail.com>
2024-02-20 10:51:23 +0300

    update

---
+++ mese/EkranIothook/Form2.cs
@@ -0,0 +1,139 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace EkranIothook
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+
+            // DateTime startDate;
+            // DateTime endDate;
+
+            string startDate = textBox1.Text;
+            string endDate = textBox2.Text;
+            string apiKey = "1c1094835fe305ad04096223";
+
+            string apiUrl = $"https://iothook.com/api/device/?api_key=1c1094835fe305ad04096223";
+
+            using (HttpClient httpClient = new HttpClient())
+            {
+                try
+                {
+                    HttpResponseMessage response = await httpClient.GetAsync(apiUrl);
+
+                    if (response.IsSuccessStatusCode)
+                    {
+                        string responseData = await response.Content.ReadAsStringAsync();
+
+                        MessageBox.Show(responseData);
+                    }
+                    else
+                    {
+                        MessageBox.Show("HTTP Error: " + response.StatusCode);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error: " + ex.Message);
+                }
+            }
+
+
+
+            Form3 fm3 = new Form3();
+            fm3.Show();
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private async void Form2_Load(object sender, EventArgs e)
+        {
+            HttpClient httpClient = new HttpClient();
+
+            // HttpRequestMessage oluşturulması
+            HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Get, "https://iothook.com/api/device/?api_key=1c1094835fe305ad04096223&results=1");
+            request.Headers.Add("api_key", "1c1094835fe305ad04096223");
+
+            HttpResponseMessage response = await httpClient.SendAsync(request);
+
+            var responseString = await response.Content.ReadAsStringAsync();
+            var statusCode = response.StatusCode;
+
+            responseString = responseString.Replace("[", "").Replace("]", "");
+
+            IothookMember member = JsonConvert.DeserializeObject<IothookMember>(responseString);
+            string field_4 = member.field_4;
+            string field_5 = member.field_5;
+            string field_6 = member.field_6;
+
+            MessageBox.Show("Kırmızı: " + field_4 + " Yeşil: " + field_5 + " Sarı: " + field_6);
+            
+            // JSON formatına dönüştürülmüş veriyi göster
+            //  MessageBox.Show(responseString);
+        }
+
+        public class IothookMember
+        {
+            public string field_4 { get; set; }
+            public string field_5 { get; set; }
+            public string field_6 { get; set; }
+        }
+
+        public class Program
+        {
+            public static async Task Main()
+            {
+                HttpClient httpClient = new HttpClient();
+
+                // HttpRequestMessage oluşturulması
+                HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Get, "https://iothook.com/api/device/?api_key=1c1094835fe305ad04096223&results=2");
+                request.Headers.Add("api_key", "1c1094835fe305ad04096223");
+
+                HttpResponseMessage response = await httpClient.SendAsync(request);
+
+                string responseString = await response.Content.ReadAsStringAsync();
+                var statusCode = response.StatusCode;
+
+            //     MessageBox.Show(responseString);
+
+                // string jsonString = "{\"Name\":\"John Doe\",\"Bio\":\"Software developer\",\"JoinDate\":\"2023-05-31T20:29:33-04:00\",\"Author\":true}";
+                // IothookMember member = JsonSerializer.Deserialize<IothookMember>(jsonString);
+
+
+            //     Console.WriteLine($"Field1: {member.Field1}");
+               // Console.WriteLine($"Field2: {member.Field2}");
+               // Console.WriteLine($"Field3: {member.Field3}");
+            }
+        }
+    }
+}
+
+
+
---
+++ mese/EkranIothook/Form3.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace EkranIothook
+{
+    partial class Form3
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea3 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend3 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Title title3 = new System.Windows.Forms.DataVisualization.Charting.Title();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea4 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend4 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series4 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Title title4 = new System.Windows.Forms.DataVisualization.Charting.Title();
+            this.chartGraph = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.chart2 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.chartGraph)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chartGraph
+            // 
+            chartArea3.Name = "ChartArea1";
+            this.chartGraph.ChartAreas.Add(chartArea3);
+            legend3.Name = "Legend1";
+            this.chartGraph.Legends.Add(legend3);
+            this.chartGraph.Location = new System.Drawing.Point(28, 35);
+            this.chartGraph.Name = "chartGraph";
+            series3.ChartArea = "ChartArea1";
+            series3.Legend = "Legend1";
+            series3.Name = "Yanma Sayısı";
+            this.chartGraph.Series.Add(series3);
+            this.chartGraph.Size = new System.Drawing.Size(459, 411);
+            this.chartGraph.TabIndex = 0;
+            this.chartGraph.Text = "chart1";
+            title3.Name = "Graph";
+            title3.Text = "Chart Graph";
+            this.chartGraph.Titles.Add(title3);
+            // 
+            // chart2
+            // 
+            chartArea4.Name = "ChartArea1";
+            this.chart2.ChartAreas.Add(chartArea4);
+            legend4.Name = "Legend1";
+            this.chart2.Legends.Add(legend4);
+            this.chart2.Location = new System.Drawing.Point(505, 35);
+            this.chart2.Name = "chart2";
+            series4.ChartArea = "ChartArea1";
+            series4.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Pie;
+            series4.Legend = "Legend1";
+            series4.Name = "Yanma Sayısı";
+            this.chart2.Series.Add(series4);
+            this.chart2.Size = new System.Drawing.Size(464, 411);
+            this.chart2.TabIndex = 1;
+            this.chart2.Text = "chart2";
+            title4.Name = "Chart Circle";
+            title4.Text = "Chart Circle";
+            this.chart2.Titles.Add(title4);
+            this.chart2.Click += new System.EventHandler(this.chart2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.Location = new System.Drawing.Point(859, 476);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(110, 36);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Yükle";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form3
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(998, 563);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.chart2);
+            this.Controls.Add(this.chartGraph);
+            this.Name = "Form3";
+            this.Text = "Form3";
+            this.Load += new System.EventHandler(this.Form3_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.chartGraph)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chartGraph;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart2;
+        private System.Windows.Forms.Button button1;
+    }
+}
